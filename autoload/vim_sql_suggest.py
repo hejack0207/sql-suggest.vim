347baaeb9d13963f6eb25868c1a99f114dbbe087
hejack0207 <hejack0207@sina.com>
2017-07-28 10:08:52 +0800

    function get_tables done

--- autoload/vim_sql_suggest.py
+++ autoload/vim_sql_suggest.py
@@ -1,20 +1,21 @@
 from string import find
 import subprocess
 
-ORACLE_OUTPUT_FILTER = """ | head -n -5 | tail -n +14 | grep -v '^$' | grep -v '\-\{30\}' | grep -v 'TABLE_NAME' """
-ORACLE_TABLES_QUERY= """{0} 2> /dev/null <<< "select table_name from all_user_table where schema_name = '{1}';" | head """ + ORACLE_OUTPUT_FILTER
+ORACLE_OUTPUT_FILTER = ""
+ORACLE_OUTPUT_FILTER = """ | head -n -5 | tail -n +14 | grep -v '^$' | grep -v '"""+'\-'*30+"""' | grep -v 'TABLE_NAME' """
+ORACLE_TABLES_QUERY= """{0} 2> /dev/null <<< "select table_name from user_tables;" """ + ORACLE_OUTPUT_FILTER
+#ORACLE_TABLES_QUERY= """{0} <<< "select table_name from user_tables;" """ + ORACLE_OUTPUT_FILTER
 ORACLE_COLUMNS_QUERY= """{0} 2> /dev/null <<< "select column_name from  all_user_columns where table_name = '{1}';" """ + ORACLE_OUTPUT_FILTER
 
-PSQL_TABLES_QUERY= """{0} -c "select tablename from pg_tables where schemaname = 'public'" {1} 2> /dev/null """
+PSQL_TABLES_QUERY= """{0} -c "select tablename from pg_tables where schemaname = 'public'" 2> /dev/null """
 PSQL_COLUMNS_QUERY= """{0} -c "select column_name from information_schema.columns where table_name = {1}" 2> /dev/null """
-MYSQL_TABLES_QUERY= """{0} -e 'SHOW tables;' {1} 2> /dev/null """
+MYSQL_TABLES_QUERY= """{0} -e 'SHOW tables;' 2> /dev/null """
 MYSQL_COLUMNS_QUERY= """{0} -e 'SHOW COLUMNS FROM {1}' 2> /dev/null """
 
 
 def get_table_names(suggest_db):
     get_tables_query, _ = get_db_specific_query_statements(suggest_db)
-    schema_name = "schema"
-    query_string = get_tables_query.format(suggest_db, schema_name)
+    query_string = get_tables_query.format(suggest_db)
     print "query string:"+query_string
     tables = check_command_output(query_string)
     db_type = get_db_type(suggest_db)
@@ -32,13 +33,6 @@ def get_column_names(suggest_db, word_to_complete):
     else:
         return create_column_name_list(suggest_db, get_table_names(suggest_db))
 
-def get_db_type(suggest_db):
-    db_type = suggest_db.split(" ")[0]
-    if find(db_type,"sqlplus") != 0:
-        return db_type
-    else:
-        return "oracle"
-
 def get_db_specific_query_statements(suggest_db):
     queries = {
         "oracle_tables": ORACLE_TABLES_QUERY,
@@ -51,8 +45,16 @@ def get_db_specific_query_statements(suggest_db):
     db_type = get_db_type(suggest_db)
     return (queries[db_type + "_tables"], queries[db_type + "_columns"])
 
+def get_db_type(suggest_db):
+    db_type = suggest_db.split(" ")[0]
+    if find(db_type,"sqlplus") != 0:
+        return db_type
+    else:
+        return "oracle"
+
 def check_command_output(query_string):
-    return subprocess.check_output(query_string, shell=True)
+    #return subprocess.check_output(query_string, shell=True)
+    return subprocess.check_output(query_string, shell=True, executable="/bin/bash")
 
 def create_column_name_list(suggest_db, tables, prefix=""):
     table_cols = []
@@ -70,3 +72,8 @@ def create_column_name_list(suggest_db, tables, prefix=""):
             table_cols.extend([{"word": prefix + column.strip(), "menu": table, "dup": 1} for column in columns.rstrip().split("\n")[2:-1]])
     return table_cols
 
+if __name__ == "__main__":
+    #print "{0}:{1}".format("name","value")
+    #print ORACLE_TABLES_QUERY
+    #print ORACLE_TABLES_QUERY.format("name","value")
+    print get_table_names('sqlplus64 "gjzspt/12345678@192.168.21.249/gjzs"')
